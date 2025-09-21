b8777a9c987dc2df0fb1e0240e815d5b4fd914fb
Miguel Faxas-Flores Guzman <me.faxas@gmail.com>
2025-09-21 23:20:38 +0000

    So far i think everything is working as it should. I don't know if there

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,34 @@
 const { FlatCompat } = require("@eslint/eslintrc");
 const path = require("path");
+const reactRecommended = require("eslint-plugin-react/configs/recommended");
+const tsRecommended = require("@typescript-eslint/eslint-plugin/dist/configs/recommended.js");
 
 const compat = new FlatCompat({
     baseDirectory: __dirname,
 });
 
 module.exports = [
-  ...compat.extends("next/core-web-vitals", "next/typescript")
+    ...compat.extends("next/core-web-vitals"),
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        ...reactRecommended,
+        ...tsRecommended,
+        rules: {
+            ...reactRecommended.rules,
+            ...tsRecommended.rules,
+            "react/react-in-jsx-scope": "off",
+            "react/prop-types": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }
+            ],
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/ban-ts-comment": "off",
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+    }
 ];
