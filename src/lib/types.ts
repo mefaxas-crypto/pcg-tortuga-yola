d0cd0a8ada53298512240fee697479eb8eaf6ae9
Miguel Faxas-Flores Guzman <me.faxas@gmail.com>
2025-09-21 04:14:32 +0000

    Love it, lets make that log all production functional

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -98,8 +98,11 @@ export type Sale = {
 export type AddSaleData = Omit<Sale, 'id'>;
 
 export type ProductionItem = {
-  recipeId: string; // The ID of the sub-recipe to produce
-  quantityProduced: number; // How many batches/yields were made
+  recipeId: string;
+  name: string;
+  yield: number;
+  yieldUnit: string;
+  quantityProduced: number;
 };
 
 export type LogProductionData = {
