b10f312696e1fabacacd04ea16094691f01746f2
Miguel Faxas-Flores Guzman <me.faxas@gmail.com>
2025-09-25 21:58:53 +0000

    yes please

--- backend/core/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import PantryItem
-
-class PantryItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PantryItem
-        fields = '__all__'
