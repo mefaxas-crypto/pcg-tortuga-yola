6668a7741fda2e14473eae79132b7c1e6cc39fda
Miguel Faxas-Flores Guzman <me.faxas@gmail.com>
2025-09-26 11:58:49 +0000

    Try fixing this error: `Runtime TypeError: Cannot read properties of und

--- src/app/dashboard/components/LowStockItems.tsx
+++ src/app/dashboard/components/LowStockItems.tsx
@@ -94,11 +94,14 @@ export function LowStockItems({ showTable = false }: LowStockItemsProps) {
     
     return lowStockLevels.map(stock => {
       const spec = lowStockItemSpecs.find(s => s.id === stock.inventoryId);
+      if (!spec) return null; // If spec is not found, return null
       return {
         ...spec,
         ...stock,
       } as InventoryItem & InventoryStockItem;
-    }).sort((a,b) => a.name.localeCompare(b.name));
+    })
+    .filter(Boolean) // Filter out the null values
+    .sort((a,b) => a.name.localeCompare(b.name));
   }, [lowStockItemSpecs, lowStockLevels]);
 
   if (showTable) {
